dac4edc3d4440e3d1f9160ac2b22d3865423e44c
samiullahtufail <sami.u.tufail@samiullahtufails-MacBook-Pro.local>
2023-04-18 09:53:29 +0500

    the new changes inclussion of index.js file

---
+++ index.js
@@ -0,0 +1,18 @@
+const hamBurger = document.querySelector('#nav-button');
+const cloSerX = document.querySelector('#close-navlist');
+const mariMarzi = document.querySelector('#nav-list')
+const navListItems = document.querySelectorAll('#nav-list > ul > li');
+hamBurger.addEventListener('click', () => {
+  mariMarzi.classList.remove('hide');
+  mariMarzi.classList.add('mobile-menu');
+});
+cloSerX.addEventListener('click', () => {
+  mariMarzi.classList.add('hide');
+  mariMarzi.classList.remove('mobile-menu');
+});
+
+navListItems.forEach((item) => item.addEventListener('click', () => {
+  mariMarzi.classList.add('hide');
+  mariMarzi.classList.remove('mobile-menu');
+}));
+
